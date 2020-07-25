d1e209e56e5089d7630ace6201d896a9179a5799
Uchencho <aloziekelechi17@gmail.com>
2020-07-25 17:34:28 +0100

    comcluded the auth product

--- auth/auth.go
+++ auth/auth.go
@@ -14,6 +14,15 @@ type Initializer struct {
 	baseurl string
 }
 
+type genPayload struct {
+	Page       string `json:"page"`
+	Limit      string `json:"limit"`
+	CustomerID string `json:"customer"`
+	From       string `json:"from"`
+	To         string `json:"to"`
+	BankID     string `json:"bank"`
+}
+
 // New creates a new struct
 func New(t string, b string) Initializer {
 	u := Initializer{
@@ -68,7 +77,7 @@ func (w Initializer) RetrieveAuth() (body string, err error) {
 
 }
 
-// ByID fetch authentication info using the id of the authentication record.
+// ByID fetches authentication info using the id of the authentication record.
 func (w Initializer) ByID(i string) (body string, err error) {
 
 	reqBody, err := json.Marshal(map[string]string{
@@ -87,7 +96,7 @@ func (w Initializer) ByID(i string) (body string, err error) {
 	return
 }
 
-// ByOptions fetch authentication info using the options metadata you provided when setting up the widget.
+// ByOptions fetches authentication info using the options metadata you provided when setting up the widget.
 func (w Initializer) ByOptions(page string, limit string, firstname string, lastname string) (body string, err error) {
 
 	type Option struct {
@@ -123,21 +132,93 @@ func (w Initializer) ByOptions(page string, limit string, firstname string, last
 	return
 }
 
-// ByCustomer fetch authentication info using the customer id
-func (w Initializer) ByCustomer(customerID string) (body string, err error) {
+// ByCustomer fetches authentication info using the customer id
+func (w Initializer) ByCustomer(page string, limit string, customerID string) (body string, err error) {
 
-	reqBody, err := json.Marshal(map[string]string{
-		"customer": customerID,
-	})
+	pl := genPayload{
+		Page:       page,
+		Limit:      limit,
+		CustomerID: customerID,
+	}
+
+	reqBody, err := json.Marshal(pl)
 	if err != nil {
 		return "Error", fmt.Errorf("error marshalling json: %w", err)
 	}
 
 	endpoint := w.baseurl + "auth/getByCustomer"
-
 	body, err = postRequest(endpoint, reqBody, w.token)
 	if err != nil {
 		return "Error", fmt.Errorf("error retrieving auth bycustomer: %w", err)
+	}
+	return
+}
+
+// ByDateRange fetches authentication info using a date range.
+func (w Initializer) ByDateRange(page string, limit string, from string, to string) (body string, err error) {
+
+	pl := genPayload{
+		Page:  page,
+		Limit: limit,
+		From:  from,
+		To:    to,
+	}
+
+	reqBody, err := json.Marshal(pl)
+	if err != nil {
+		return "Error", fmt.Errorf("error marshalling json: %w", err)
+	}
+
+	endpoint := w.baseurl + "auth/getByDate"
+	body, err = postRequest(endpoint, reqBody, w.token)
+	if err != nil {
+		return "Error", fmt.Errorf("error retrieving auth byDateRange: %w", err)
+	}
+	return
+}
+
+// ByBank fetches authentication info using the bank id.
+func (w Initializer) ByBank(page string, limit string, bankID string) (body string, err error) {
+
+	pl := genPayload{
+		Page:   page,
+		Limit:  limit,
+		BankID: bankID,
+	}
+
+	reqBody, err := json.Marshal(pl)
+	if err != nil {
+		return "Error", fmt.Errorf("error marshalling json: %w", err)
+	}
+
+	endpoint := w.baseurl + "auth/getByBank"
+	body, err = postRequest(endpoint, reqBody, w.token)
+	if err != nil {
+		return "Error", fmt.Errorf("error retrieving auth byBank: %w", err)
+	}
+	return
+}
+
+// ByCustomerDate authentication for a customer using a date range and customer id.
+func (w Initializer) ByCustomerDate(page string, limit string, from string, to string, customerID string) (body string, err error) {
+
+	pl := genPayload{
+		Page:       page,
+		Limit:      limit,
+		From:       from,
+		To:         to,
+		CustomerID: customerID,
+	}
+
+	reqBody, err := json.Marshal(pl)
+	if err != nil {
+		return "Error", fmt.Errorf("error marshalling json: %w", err)
+	}
+
+	endpoint := w.baseurl + "auth/getByDateCustomer"
+	body, err = postRequest(endpoint, reqBody, w.token)
+	if err != nil {
+		return "Error", fmt.Errorf("error retrieving auth byCustomerDate: %w", err)
 	}
 	return
 }
